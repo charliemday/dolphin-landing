884ecfdac3fbc870f96c05181283d6731b1844db
charliemday <45036245+charliemday@users.noreply.github.com>
2023-12-30 12:02:47 +0000

    Updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,33 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+// import { defineConfig } from 'astro/config';
+// import tailwind from "@astrojs/tailwind";
+// import sitemap from "@astrojs/sitemap";
+// export default defineConfig({
+//    site: 'https://lexingtonthemes.com',
+//   integrations: [tailwind(),  sitemap()]
+// });
+
+// import image from "@astrojs/image";
+// import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+// import vercel from "@astrojs/vercel/static";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  site: "https://astroship.web3templates.com",
+  // output: "server",
+  // adapter: vercel({
+  //   webAnalytics: {
+  //     enabled: true,
+  //   },
+  // }),
+  integrations: [
+    tailwind(),
+    // image({
+    //   serviceEntryPoint: "@astrojs/image/sharp",
+    // }),
+    // mdx(),
+    sitemap(),
+  ],
 });
