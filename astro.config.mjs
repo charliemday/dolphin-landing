e440fc12b61fc9be39fd7152024c349ea1fc64c2
charliemday <45036245+charliemday@users.noreply.github.com>
2024-01-01 14:14:35 +0000

    Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,18 +10,18 @@
 // import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-// import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   // output: "server",
-  // adapter: vercel({
-  //   webAnalytics: {
-  //     enabled: true,
-  //   },
-  // }),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [
     tailwind(),
     // image({
